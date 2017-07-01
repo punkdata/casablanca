13786655b4a04966f48897998dafe3d4e3205214
Angel Rivera <arivera@point.io>
2017-07-01 09:46:46 -0400

    added cp cmd

--- build_docker.sh
+++ build_docker.sh
@@ -5,4 +5,5 @@ output=$(aws --profile datapunks ecr get-login --region us-east-1 ) && $output
 docker build -t casablanca .
 docker tag casablanca:latest 146563464137.dkr.ecr.us-east-1.amazonaws.com/casablanca:latest
 docker push 146563464137.dkr.ecr.us-east-1.amazonaws.com/casablanca:latest
+cp config.json ~/Dropbox/stuff/creds/casablanca/config.json
 echo "the docker build is complete"
